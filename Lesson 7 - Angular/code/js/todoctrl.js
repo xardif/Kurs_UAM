c4a9f57e7a20f5fb2de873c8090796a97b321020
Xardif <xardif@gmail.com>
2015-04-08 21:30:43 +0200

    Solution for Lesson 7

--- Lesson 7 - Angular/code/js/todoctrl.js
+++ Lesson 7 - Angular/code/js/todoctrl.js
@@ -1,6 +1,8 @@
 angular.module('myapp', []).controller('TodoCtrl', function ($scope) {
 	$scope.elements = [];
 	$scope.inputValue = '';
+	$scope.total = 0;
+	$scope.active = 0;
 
 	$scope.addNew = function () {
 		if (!$scope.inputValue) {
@@ -10,6 +12,22 @@ angular.module('myapp', []).controller('TodoCtrl', function ($scope) {
 			value: $scope.inputValue,
 			status: true
 		});
+		$scope.total++;
+		$scope.active++;
 		$scope.inputValue = '';
 	};
+
+	$scope.elementClicked = function($element){
+		$scope.active += ($element.status) ? -1 : 1;
+		$element.status = !$element.status;
+	}
+
+	$scope.deleteElement = function($element){
+		var index = $scope.elements.indexOf($element);
+		$scope.elements.splice(index, 1);
+		if($element.status){
+			$scope.active--;
+		}
+		$scope.total--;
+	}
 });
--- Lesson 7 - Angular/code2/js/ctrl.js
+++ Lesson 7 - Angular/code2/js/ctrl.js
@@ -1,7 +1,18 @@
-angular.module('myapp').controller('MyCtrl', ["$scope", "deps1", function ($sc, dp1) {
+angular.module('myapp').controller('MyCtrl', ["$scope", "clearService", function ($sc, clearService) {
 	$sc.name = "Gucio";
 
-	$sc.clear = function () {
-		$sc.name = '';
-	};
+	$sc.clear =	function(){
+		clearService.clear($sc, "name");
+	}
 }]);
+
+
+
+
+angular.module('myapp').factory('clearService', function () {
+  return {
+		clear: function (scope, variable) {
+			scope[variable] = '';
+		}
+  };
+});
