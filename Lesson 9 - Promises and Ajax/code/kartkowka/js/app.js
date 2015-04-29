c2d65412401e9357ac6f488d4fe0aae779cb34cf
Xardif <xardif@gmail.com>
2015-04-29 09:24:45 +0200

    Solution for Lesson 9 - 1

--- Lesson 9 - Promises and Ajax/code/kartkowka/js/app.js
+++ Lesson 9 - Promises and Ajax/code/kartkowka/js/app.js
@@ -1,1 +1,28 @@
-angular.module('app', [])
+angular.module('myapp', []).controller('CarCtrl', ["$scope", function ($scope) {
+  $scope.additions = [];
+
+  $scope.add = function(){
+    $scope.additions.push({
+      color: "",
+      option: ""
+    });
+  };
+}]);
+
+angular.module('myapp').directive('carAddition', function () {
+	return {
+		restrict: 'E',
+		scope: {
+			param: "="
+		},
+		templateUrl: 'templates/addition.html',
+    link: function ($scope, $element, $attrs) {
+      $scope.options = ["Spoiler", "Progi boczne", "Lusterka", "Felgi"];
+
+      $scope.updateColor = function($color){
+        $scope.param.color = $color;
+      };
+
+		}
+	};
+});
